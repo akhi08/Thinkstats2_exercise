d72f97953c0d2e003037500f25fabe2a9990050c
akhi <akhileshdangi7@gmail.com>
2025-08-08 20:50:32 +0530

    ohe logres

--- Thinkstats/Approach any problem with ml/mnist_classifier/src/create_folds.py
+++ Thinkstats/Approach any problem with ml/mnist_classifier/src/create_folds.py
@@ -1,19 +1,19 @@
 import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 
-if __name__=="__main__":
-    df=pd.read_csv('/Users/akhichoudhary/STATS/Thinkstats/Thinkstats2_exercise/Thinkstats/Approach any problem with ml/mnist_classifier/input/mnist_train.csv')
-    # Shuffle data
-    df=df.sample(frac=1).reset_index(drop=True)
-    # Create a new column for fold  assignment
-    df['kfold']=-1
-    # Initialize stratified k-fold (assuming 'label' column exists)
-    skf=StratifiedKFold(n_splits=5)
-    # assign folds
-    for fold,(train_idx,val_idx) in enumerate(skf.split(X=df,y=df.label)):
-        df.loc[val_idx,'kfold']=fold
-    #Save the new csv
-    df.to_csv("input/mnist_train_folds.csv", index=False)
+# if __name__=="__main__":
+#     df=pd.read_csv('/Users/akhichoudhary/STATS/Thinkstats/Thinkstats2_exercise/Thinkstats/Approach any problem with ml/mnist_classifier/input/mnist_train.csv')
+#     # Shuffle data
+#     df=df.sample(frac=1).reset_index(drop=True)
+#     # Create a new column for fold  assignment
+#     df['kfold']=-1
+#     # Initialize stratified k-fold (assuming 'label' column exists)
+#     skf=StratifiedKFold(n_splits=5)
+#     # assign folds
+#     for fold,(train_idx,val_idx) in enumerate(skf.split(X=df,y=df.label)):
+#         df.loc[val_idx,'kfold']=fold
+#     #Save the new csv
+#     df.to_csv("input/mnist_train_folds.csv", index=False)
 
 
 # StratifiedKFold ensures each fold has roughly the same percentage of each class label.
@@ -158,6 +158,29 @@ if __name__=="__main__":
 # valid_df = df[df.kfold == 0]
 
 
+############### =====================================================this code is in approaching categorical value notebook 
+# ########/Users/akhichoudhary/STATS/Thinkstats/Thinkstats2_exercise/Thinkstats/Approach any problem with ml
+#########  to practice on cat-in-data
 
+# import pandas as pd
+# from skelean import model_selection
 
+import pandas as pd
+from sklearn import model_selection
+if __name__ == "__main__":
+    # Read training data
+    df = pd.read_csv("/Users/akhichoudhary/STATS/Thinkstats/Thinkstats2_exercise/Thinkstats/Approach any problem with ml/input/cat-in-data/train.csv")
+    # we create a new column called kfold and fill it with -1
+    df["kfold"] = -1
+    # the next step is to randomize the rows of the data
+    df = df.sample(frac=1).reset_index(drop=True)
+    # fetch labels
+    y = df.target.values
+    # initiate the kfold class from model_selection module
+    kf = model_selection.StratifiedKFold(n_splits=5)
+    # fill the new kfold column
+    for f, (t_, v_) in enumerate(kf.split(X=df, y=y)):
+        df.loc[v_, 'kfold'] = f
+    # save the new csv with kfold column
+    df.to_csv('input/cat-in-data/cat_train_folds.csv')
 
